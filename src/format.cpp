fc307d9e153e8a568cd54acef0a991c8e17ec7ff
krugm421 <krug.m42@gmail.com>
2021-12-04 11:24:42 +0100

    Rework for the time formating

--- src/format.cpp
+++ src/format.cpp
@@ -1,6 +1,10 @@
 #include <string>
-
 #include "format.h"
+
+
+#include <sstream>
+#include <iostream>
+#include <fstream>
 
 using std::string;
 using std::to_string;
@@ -12,8 +16,15 @@ string Format::ElapsedTime(long seconds) {
     long full_hours = (long)(seconds / 3600);
     long full_minutes = (long)((seconds % 3600) / 60);
     long full_seconds = (long)((seconds % 3600) % 60);
-    
-    
-    return to_string(full_hours) + ":" + to_string(full_minutes) + ":" + to_string(full_seconds); 
+
+
+    string full_hours_str = std::to_string(full_hours);
+    string full_minutes_str = std::to_string(full_minutes);
+    string full_seconds_str = std::to_string(full_seconds);
+    string time = string(2 - full_hours_str.length(), '0') + full_hours_str + ':' +
+                string(2 - full_minutes_str.length(), '0') + full_minutes_str + ':' +
+                string(2 - full_seconds_str.length(), '0') + full_seconds_str;
+
+    return time;
 }
 
